95ede4e2c3f65ce7f3b38f9dd82f5b8bd6220fc2
Hadi Khani <hadikhani40@gmail.com>
2025-03-28 14:09:11 +0330

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'calculator.js',
-    library: 'SimpleCalculator',
-    libraryTarget: 'umd',
+    library: {
+      name: 'SimpleCalculator',
+      type: 'umd',
+      export: 'default'
+    },
     globalObject: 'this'
   },
   mode: 'production',
+  experiments: {
+    outputModule: true
+  }
 };
